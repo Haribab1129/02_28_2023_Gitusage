4247dd8f2df3f0888bd61073f8d14378e70d8768
ICX <haribabu81254@gmail.com>
2023-02-28 13:08:30 +0530

    commit files

---
+++ src/main/java/com/mycompany/_28_2023_gitusage/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._28_2023_gitusage;
+
+/**
+ *
+ * @author ICX
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
